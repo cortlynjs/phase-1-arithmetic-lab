995b658f18d5f1ed2ad67496ef58b5819aee1a19
Cortlyn Stafford <cs.11twenty@gmail.com>
2023-03-13 12:31:24 -0400

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Define variables
+const num1 = 31;
+const num2 = 2;
+const num3 = 22;
+const num4 = 9;
 
-// Write your code here
+// Define functions
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100) + 1; // random integer between 1 and 100
+const mod = num3 % num4;
+const numArray = [20, 10, 15, 2];
+const max = Math.max(...numArray);
+
+
